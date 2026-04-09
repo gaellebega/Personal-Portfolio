ae0b5c43dfbcf716e7e528be38d7fe086a4b80c6
gaellebega <gaellebega@gmail.com>
2026-04-09 22:33:46 +0200

    all commit updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { RiLinkedinFill } from "react-icons/ri"
 function Footer(){
   return(
   <>
-  <Footer className="bg-[#0b1120] ">
-    <div className="inset-0 absolute flex items-center h-full divide-x divide-gray-700">
+  <Footer>
+      <div>
     <p><a>find me in:</a></p>
     <RiLinkedinFill/>
   </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,10 @@ function Home() {
           <p className="text-gray-500">Hi all. I am</p>
           <h3 className="text-4xl">ISHAMI Gaelle Bega</h3>
           <p className="text-[#4d4dff] text-2xl"><span className="ml-1">{">"}</span>Front-end developer</p>
-
           <div>
             <p className="text-gray-500">//find my profle on Github:</p>
-            <p className="text-[#4d4dff]">const <span className="text-[#19ccbd] ">githubLink</span> <a href="https://github.com/gaellebega" className="text-red-300" target="_blank">https://github.com/gaellebega</a></p>
+            <p className="text-[#4d4dff]">const <span className="text-[#19ccbd] ">githubLink</span> <a href="https://github.com/gaellebega" className="text-red-300">https://github.com/gaellebega</a></p>
           </div>
-
         </div>
 
         <div className="w-1/2 relative  bg-gradient-to-r from-[#0b1120] via-[#0f766e] to-[#4338ca] text-white p-8 ">
@@ -20,7 +18,6 @@ function Home() {
               <div>
                 <p className=" font-bold text-2xl text-[#19ccbd] drop-shadow-[0_0_8px_rgba(25,204,189,0.7)]">SNAKE GAME</p>
                 <p className="text-[#19ccbd]">PRESS TO START TO PLAY</p>
-
                 <p className="bg-[#e8a62a]  rounded-md h-10 w-16 text-[#0b1120]">start game</p>
                   <p className="  rounded-md border-1 border-white  h-10 w-16 text-white">skip game</p>
               </div>
