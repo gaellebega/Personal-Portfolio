82a11ab189b92a8fcbcfa0b41fe29f10d9fe4c79
gaellebega <gaellebega@gmail.com>
2026-04-09 14:35:00 +0200

    home-page colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
 function Home() {
   return (
     <>
-      <section className="flex">
-        <div className="w-1/2 bg-blue-600 h-screen">
-        <p className="text-gray-500">Hi all</p>
-        <h3>ISHAMI Gaelle Bega</h3>
-        <p>Front-end developer</p>
+      <section className="flex ">
+        <div className="w-1/2  bg-[#0b1120] h-screen">
+        <p className="text-gray-500">Hi all. I am</p>
+        <h3 className="text-4xl">ISHAMI Gaelle Bega</h3>
+        <p className="text-[#4d4dff] text-2xl"><span className="ml-1">{">"}</span>Front-end developer</p>
 
         <div>
-          <p>//find my profle on Github:</p>
-          <p>const<a link="https://github.com/gaellebega" ></a></p>
+          <p className="text-gray-500">//find my profle on Github:</p>
+          <p className="text-[#4d4dff]">const <span className="text-[#19ccbd]">githubLink</span> <a li="https://github.com/gaellebega" className="text-red-300">https://github.com/gaellebega</a></p>
         </div>
 
         </div>
 
-        <div className="w-1/2 bg-yellow-800 h-screen">
+        <div className="w-1/2 bg-gradient-to-r from-[#0b1120] via-[#0f766e] to-[#4338ca] text-white p-8 rounded-lg">
         right content
         </div>
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // <-- must include your JSX files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
